6ed5af320b54823a78a894d316395b3c5315a6a2
Rajat Venkatesh <vrajat@dblint.io>
2018-09-06 22:23:52 +0530

    new:usr:Add a query analyzer module
    query analyzer module parses a sql query and provides
    a framework to write visitors to analyze it. right now
    it consists of a simple query classifier.

---
+++ qan/src/main/java/Classifier.java
@@ -0,0 +1,70 @@
+import org.apache.calcite.sql.SqlCall;
+import org.apache.calcite.sql.SqlFilterOperator;
+import org.apache.calcite.sql.SqlNode;
+import org.apache.calcite.sql.SqlOperator;
+import org.apache.calcite.sql.SqlOperatorTable;
+import org.apache.calcite.sql.SqlSelect;
+import org.apache.calcite.sql.SqlSelectOperator;
+import org.apache.calcite.sql.parser.SqlParseException;
+import org.apache.calcite.sql.util.SqlBasicVisitor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Classifier {
+  public final Parser parser;
+  public final List<ClassifyingVisitor> visitors;
+  public enum QueryClass {
+    LOOKUP
+  }
+
+  abstract class ClassifyingVisitor extends SqlBasicVisitor<Void> {
+    protected final QueryClass queryClass;
+    protected boolean passed;
+
+    ClassifyingVisitor(QueryClass queryClass, boolean passed) {
+      this.queryClass = queryClass;
+      this.passed = passed;
+    }
+
+    public boolean isPassed() {
+      return passed;
+    }
+  }
+
+  class LookupVisitor extends ClassifyingVisitor {
+    LookupVisitor() {
+      super(QueryClass.LOOKUP, true);
+    }
+
+    public Void visit(SqlCall sqlCall) {
+      if (sqlCall instanceof SqlSelect) {
+        SqlSelect sqlSelect = (SqlSelect) sqlCall;
+        if (sqlSelect.isDistinct()
+          || sqlSelect.hasOrderBy()) {
+          this.passed = false;
+        }
+      }
+      return super.visit(sqlCall);
+    }
+  }
+
+  Classifier() {
+    this.parser = new Parser();
+    this.visitors = new ArrayList<>();
+    this.visitors.add(new LookupVisitor());
+  }
+
+  public List<QueryClass> classify(String sql) throws SqlParseException {
+    List<QueryClass> classList = new ArrayList<>();
+    SqlNode parseTree = parser.parse(sql);
+    for (ClassifyingVisitor visitor: visitors) {
+      parseTree.accept(visitor);
+      if (visitor.isPassed()) {
+        classList.add(visitor.queryClass);
+      }
+    }
+
+    return classList;
+  }
+}
