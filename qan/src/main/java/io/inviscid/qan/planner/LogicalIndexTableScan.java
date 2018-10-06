9a607bdb9775855701a6c590aad159e0b3b13cda
Rajat Venkatesh <inviscidtune@gmail.com>
2018-10-06 22:40:21 +0530

    new:usr:Add an optimizer and rules to detect index usage in MySQL queries
    Planner can optimizer parsed & planned SQL queries. Only one rule is currently
    active to detect index table scans if a table has indexes defined and there
    are equality filters on the index column.

---
+++ qan/src/main/java/io/inviscid/qan/planner/LogicalIndexTableScan.java
@@ -0,0 +1,68 @@
+package io.inviscid.qan.planner;
+
+import org.apache.calcite.plan.Convention;
+import org.apache.calcite.plan.RelOptCluster;
+import org.apache.calcite.plan.RelOptTable;
+import org.apache.calcite.plan.RelTraitSet;
+import org.apache.calcite.rel.RelCollationTraitDef;
+import org.apache.calcite.rel.RelNode;
+import org.apache.calcite.rel.RelWriter;
+import org.apache.calcite.rel.core.TableScan;
+import org.apache.calcite.rex.RexNode;
+import org.apache.calcite.schema.Table;
+import org.apache.calcite.util.ImmutableIntList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LogicalIndexTableScan extends TableScan {
+  public final List<RexNode> filters;
+  public final ImmutableIntList projects;
+
+  //~ Constructors -----------------------------------------------------------
+
+  /**
+   * Creates a LogicalIndexTableScan.
+   *
+   * <p>Use {@link #create} unless you know what you're doing.
+   */
+  public LogicalIndexTableScan(RelOptCluster cluster, RelTraitSet traitSet, RelOptTable table,
+                               List<RexNode> filters, ImmutableIntList projects) {
+    super(cluster, traitSet, table);
+    this.filters = filters;
+    this.projects = projects;
+  }
+
+  @Override public RelNode copy(RelTraitSet traitSet, List<RelNode> inputs) {
+    assert traitSet.containsIfApplicable(Convention.NONE);
+    assert inputs.isEmpty();
+    return this;
+  }
+
+  /** Creates a LogicalIndexTableScan.
+   *
+   * @param cluster Cluster
+   * @param relOptTable Table
+   */
+  public static LogicalIndexTableScan create(RelOptCluster cluster,
+                                             final RelOptTable relOptTable,
+                                             List<RexNode> filters,
+                                             ImmutableIntList projects) {
+    final Table table = relOptTable.unwrap(Table.class);
+    final RelTraitSet traitSet =
+        cluster.traitSetOf(Convention.NONE)
+            .replaceIfs(RelCollationTraitDef.INSTANCE, () -> {
+              if (table != null) {
+                return table.getStatistic().getCollations();
+              }
+              return new ArrayList<>();
+            });
+    return new LogicalIndexTableScan(cluster, traitSet, relOptTable, filters, projects);
+  }
+
+  @Override
+  public RelWriter explainTerms(RelWriter pw) {
+    return super.explainTerms(pw)
+        .itemIf("conditions", this.filters, true);
+  }
+}
--- qan/src/main/java/io/inviscid/qan/planner/MartTable.java
+++ qan/src/main/java/io/inviscid/qan/planner/MartTable.java
@@ -1,15 +1,12 @@
 package io.inviscid.qan.planner;
 
-import org.apache.calcite.adapter.enumerable.EnumerableTableScan;
 import org.apache.calcite.adapter.java.JavaTypeFactory;
-import org.apache.calcite.linq4j.AbstractEnumerable;
-import org.apache.calcite.linq4j.Enumerable;
-import org.apache.calcite.linq4j.Enumerator;
 import org.apache.calcite.linq4j.QueryProvider;
 import org.apache.calcite.linq4j.Queryable;
 import org.apache.calcite.linq4j.tree.Expression;
 import org.apache.calcite.plan.RelOptTable;
 import org.apache.calcite.rel.RelNode;
+import org.apache.calcite.rel.logical.LogicalTableScan;
 import org.apache.calcite.rel.type.RelDataType;
 import org.apache.calcite.rel.type.RelDataTypeFactory;
 import org.apache.calcite.schema.QueryableTable;
@@ -17,6 +14,7 @@ import org.apache.calcite.schema.SchemaPlus;
 import org.apache.calcite.schema.Schemas;
 import org.apache.calcite.schema.TranslatableTable;
 import org.apache.calcite.schema.impl.AbstractTable;
+import org.apache.calcite.util.ImmutableIntList;
 import org.apache.calcite.util.Pair;
 
 import org.slf4j.Logger;
@@ -34,6 +32,8 @@ public class MartTable extends AbstractTable
   protected final MartSchema schema;
   protected final String name;
   protected final List<MartColumn> columns;
+  final Integer primaryKey;
+  final List<Integer> secondaryIndexes;
 
   /**
    * Create a MartTable that stores information about a table.
@@ -42,21 +42,46 @@ public class MartTable extends AbstractTable
    * @param columns List of columns of type MartColumn
    */
   public MartTable(MartSchema schema, String name, List<MartColumn> columns) {
-    this.schema = schema;
-    this.name = name;
-    this.columns = columns;
+    this(schema, name, columns, null, new ArrayList<>());
   }
 
   /**
-   * Returns an enumerable over a given projection of the fields.
-   * Called from generated code.
+   * Create a MartTable that stores information about a table.
+   * Also set key/index information.
+   * @param schema Schema of the table
+   * @param name Name of the table
+   * @param columns List of columns of the table
+   * @param primaryKey Primary key of the table
    */
-  public Enumerable<Object> project(final int[] fields) {
-    return new AbstractEnumerable<Object>() {
-      public org.apache.calcite.linq4j.Enumerator enumerator() {
-        return new MartEnumerator();
-      }
-    };
+  public MartTable(MartSchema schema, String name, List<MartColumn> columns,
+                   MartColumn primaryKey) {
+    this(schema, name, columns, primaryKey, new ArrayList<>());
+  }
+
+  /**
+   * Create a MartTable that stores information about a table.
+   * Also set key/index information.
+   * @param schema Schema of the table
+   * @param name Name of the table
+   * @param columns List of columns of the table
+   * @param primaryKey Primary key of the table
+   * @param secondaryIndexes List of secondary indexes
+   */
+  public MartTable(MartSchema schema, String name, List<MartColumn> columns,
+                   MartColumn primaryKey, List<MartColumn> secondaryIndexes) {
+    this.schema = schema;
+    this.name = name;
+    this.columns = columns;
+    this.secondaryIndexes = new ArrayList<>();
+    if (primaryKey != null) {
+      this.primaryKey = getFieldOrdinal(primaryKey.name);
+      this.secondaryIndexes.add(this.primaryKey);
+    } else {
+      this.primaryKey = null;
+    }
+    for (MartColumn column : secondaryIndexes) {
+      this.secondaryIndexes.add(getFieldOrdinal(column.name));
+    }
   }
 
   @Override
@@ -80,8 +105,7 @@ public class MartTable extends AbstractTable
   public RelNode toRel(
       RelOptTable.ToRelContext context,
       RelOptTable relOptTable) {
-    // Request all fields.
-    return EnumerableTableScan.create(context.getCluster(), relOptTable);
+    return LogicalTableScan.create(context.getCluster(), relOptTable);
   }
 
   @Override
@@ -102,6 +126,10 @@ public class MartTable extends AbstractTable
 
   public List<MartColumn> getColumns() {
     return columns;
+  }
+
+  public ImmutableIntList getKeyOrdinals() {
+    return ImmutableIntList.copyOf(this.secondaryIndexes);
   }
 
   /**
@@ -139,25 +167,6 @@ public class MartTable extends AbstractTable
   @Override
   public int hashCode() {
     return schema.hashCode() + name.hashCode() * 31 + columns.hashCode() * 47;
-  }
-
-  class MartEnumerator implements Enumerator<Object> {
-    MartEnumerator() {}
-
-    public Object current() {
-      return null;
-    }
-
-    public boolean moveNext() {
-      return false;
-    }
-
-    public void reset() {
-      throw new UnsupportedOperationException();
-    }
-
-    public void close() {
-    }
   }
 }
 
