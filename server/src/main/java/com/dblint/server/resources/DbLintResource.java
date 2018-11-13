8695d864ddd690d334347d5412b770d273910f75
Rajat Venkatesh <inviscidtune@gmail.com>
2018-11-13 12:22:37 +0530

    fix:usr:Change API url to /api/
    Add /api namespace to dblint. This is required as Google Ingress
    does not filter api namespace when redirecting traffic.

--- server/src/main/java/com/dblint/server/resources/DbLintResource.java
+++ server/src/main/java/com/dblint/server/resources/DbLintResource.java
@@ -11,7 +11,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("/dblint/")
+@Path("/api/dblint/")
 @Produces(MediaType.APPLICATION_JSON)
 public class DbLintResource {
   final Parser parser;
