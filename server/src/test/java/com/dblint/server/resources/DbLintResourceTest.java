28010b73bae868ba252fbf2974f93d30ae189ea0
Rajat Venkatesh <vrajat@dblint.io>
2018-11-22 19:17:15 +0530

    new:usr:Improve Dblint resource parameters and responses
    Use classes (SqlQuery and QueryResponse) instead of strings for
    REST calls to digest and pretty print.

--- server/src/test/java/com/dblint/server/resources/DbLintResourceTest.java
+++ server/src/test/java/com/dblint/server/resources/DbLintResourceTest.java
@@ -1,9 +1,10 @@
 package com.dblint.server.resources;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import javax.ws.rs.client.Entity;
 
+import com.dblint.server.pojo.QueryResponse;
+import com.dblint.server.pojo.QueryResponseTest;
+import com.dblint.server.pojo.SqlQuery;
 import com.dblint.sqlplanner.planner.Parser;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.guava.GuavaModule;
@@ -15,6 +16,8 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(DropwizardExtensionsSupport.class)
 public class DbLintResourceTest {
@@ -41,36 +44,56 @@ public class DbLintResourceTest {
   @Test
   public void callDigestTest() throws Throwable {
     RESOURCE.before();
-    String sql = "select a from t where i = 5";
-    String response = RESOURCE.target("/api/dblint/digest")
-        .request().post(Entity.json(sql)).readEntity(String.class);
+    SqlQuery sql = new SqlQuery("select a from t where i = 5");
+    QueryResponse response = RESOURCE.target("/api/dblint/digest")
+        .request().post(Entity.json(sql)).readEntity(QueryResponse.class);
     assertEquals(
         "SELECT `A`\n" +
             "FROM `T`\n" +
-            "WHERE `I` = ?", response);
+            "WHERE `I` = ?", response.sql);
+    assertTrue(response.success);
+    assertNull(response.errorMessage);
     RESOURCE.after();
   }
 
-  @Disabled
   @Test
   public void exceptionTest() throws Throwable {
     RESOURCE.before();
-    String response = RESOURCE.target("/api/dblint/digest")
-        .request().post(Entity.json("select a from where i")).readEntity(String.class);
-    assertEquals("Redshift HighCpuEvent Capture initiated", response);
+    SqlQuery sql = new SqlQuery("select a from where i = 5");
+    QueryResponse response = RESOURCE.target("/api/dblint/digest")
+        .request().post(Entity.json(sql)).readEntity(QueryResponse.class);
+    assertEquals("Encountered \"from where\" at line 1, column 10.",
+        response.errorMessage.split("\n")[0]);
+    assertFalse(response.success);
+    assertNull(response.sql);
     RESOURCE.after();
   }
 
   @Test
   public void callPrettyTest() throws Throwable {
     RESOURCE.before();
-    String sql = "select a from t where i = 5";
-    String response = RESOURCE.target("/api/dblint/pretty")
-        .request().post(Entity.json(sql)).readEntity(String.class);
+    SqlQuery sql = new SqlQuery("select a from t where i = 5");
+    QueryResponse response = RESOURCE.target("/api/dblint/pretty")
+        .request().post(Entity.json(sql)).readEntity(QueryResponse.class);
     assertEquals(
         "SELECT `A`\n" +
             "FROM `T`\n" +
-            "WHERE `I` = 5", response);
+            "WHERE `I` = 5", response.sql);
+    assertTrue(response.success);
+    assertNull(response.errorMessage);
+    RESOURCE.after();
+  }
+
+  @Test
+  public void badDialectTest() throws Throwable {
+    RESOURCE.before();
+    SqlQuery sql = new SqlQuery("select a from t where i = 5", "someDialect");
+    QueryResponse response = RESOURCE.target("/api/dblint/pretty")
+        .request().post(Entity.json(sql)).readEntity(QueryResponse.class);
+    assertEquals("No enum constant org.apache.calcite.sql.SqlDialect.DatabaseProduct.SOMEDIALECT",
+        response.errorMessage);
+    assertFalse(response.success);
+    assertNull(response.sql);
     RESOURCE.after();
   }
 }
