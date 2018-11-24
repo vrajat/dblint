ba3389fd10354281ad81711ed885197b7cbb4c4c
Rajat Venkatesh <vrajat@dblint.io>
2018-11-24 21:43:55 +0530

    new:usr:Fix continous deploy with latest gcloud, kubectl and keys
    Install gcloud & kubectl from apt.
    Use encrypted keys to setup service accounts.

--- server/src/main/ops/deploy.sh
+++ server/src/main/ops/deploy.sh
@@ -1,22 +1,17 @@
 #!/bin/bash
 
-set -e
-
-cd $TRAVIS_BUILD_DIR/server
+set -ex
 docker build -t us.gcr.io/${PROJECT_ID_PROD}/${DOCKER_IMAGE_NAME}:$TRAVIS_TAG .
 
-echo $GCLOUD_SERVICE_KEY_PRD | base64 --decode -i > ${HOME}/gcloud-service-key.json
-gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
+gcloud auth activate-service-account --key-file server/src/main/ops/auth.json
 
-gcloud --quiet config set project $PROJECT_NAME_PROD
+gcloud --quiet config set project $PROJECT_ID_PROD
 gcloud --quiet config set container/cluster $CLUSTER_NAME_PROD
 gcloud --quiet config set compute/zone ${CLOUDSDK_COMPUTE_ZONE}
+gcloud --quiet config set container/use_application_default_credentials true
 gcloud --quiet container clusters get-credentials $CLUSTER_NAME_PROD
 
 gcloud auth configure-docker
-gcloud docker push us.gcr.io/${PROJECT_NAME_PRD}/${DOCKER_IMAGE_NAME}:$TRAVIS_TAG
+docker push us.gcr.io/${PROJECT_ID_PROD}/${DOCKER_IMAGE_NAME}:$TRAVIS_TAG
 
-kubectl config view
-kubectl config current-context
-
-# kubectl set image deployment/${KUBE_DEPLOYMENT_NAME} ${KUBE_DEPLOYMENT_CONTAINER_NAME}=us.gcr.io/${PROJECT_NAME_PRD}/${DOCKER_IMAGE_NAME}:$TRAVIS_TAG
+# kubectl set image deployment/${KUBE_DEPLOYMENT_NAME} ${KUBE_DEPLOYMENT_CONTAINER_NAME}=us.gcr.io/${PROJECT_NAME_PROD}/${DOCKER_IMAGE_NAME}:$TRAVIS_TAG
