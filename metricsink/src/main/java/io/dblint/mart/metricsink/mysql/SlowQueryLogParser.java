e87d221ee9241612fc076639981998251d9f39a5
Rajat Venkatesh <vrajat@dblint.io>
2019-03-13 11:15:23 +0530

    Change parser to a state machine type algo

--- metricsink/src/main/java/io/dblint/mart/metricsink/mysql/SlowQueryLogParser.java
+++ metricsink/src/main/java/io/dblint/mart/metricsink/mysql/SlowQueryLogParser.java
@@ -1,58 +1,69 @@
 package io.dblint.mart.metricsink.mysql;
 
-import java.io.BufferedReader;
+import io.dblint.mart.metricsink.util.MetricAgentException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class SlowQueryLogParser {
+  private static Logger logger = LoggerFactory.getLogger(SlowQueryLogParser.class);
+
   static Pattern tsLine = Pattern.compile("^# Time: ([0-9: ]{15})");
   static Pattern uhLine = Pattern.compile("# User@Host: (.*\\[.*\\])\\s*@\\s*"
       + "\\[(.*)\\]\\s*Id:\\s*(\\d+)");
   static Pattern queryMetadata = Pattern.compile("# Query_time: ([\\d\\.]+)\\s*"
       + "Lock_time: ([\\d\\.]+)\\s*Rows_sent: (\\d+)\\s*Rows_examined: (\\d+)");
-  static Pattern setStatement = Pattern.compile("SET\\s+(.*);");
+  static Pattern setStatement = Pattern.compile("SET\\s+timestamp=([0-9]+)[,|;]");
   static Pattern useStatement = Pattern.compile("USE\\s+([.;]*)");
   static Pattern comments = Pattern.compile("/\\*(.*)\\*/");
 
-  static boolean parseTsLine(String line, UserQuery userQuery) {
-    Matcher matcher = tsLine.matcher(line);
-    if (matcher.find()) {
-      userQuery.setTime(matcher.group(1));
-      return true;
-    }
-    return false;
-  }
-
-  static boolean parseUhLine(String line, UserQuery userQuery) {
+  static void parseUhLine(RewindBufferedReader reader, UserQuery userQuery)
+      throws IOException, MetricAgentException {
+    String line = reader.readLine();
     Matcher matcher = uhLine.matcher(line);
     if (matcher.find()) {
       userQuery.setUserHost(matcher.group(1));
       userQuery.setIpAddress(matcher.group(2));
       userQuery.setId(matcher.group(3));
-      return true;
+    } else {
+      throw new MetricAgentException("Line (" + reader.getLineNumber() + ") "
+          + "did not match User Header pattern: '" + line
+          + "' at " + matcher.toString());
     }
-    return false;
   }
 
-  static boolean parseQueryMetadataLine(String line, UserQuery userQuery) {
+  static void parseQueryMetadataLine(RewindBufferedReader reader, UserQuery userQuery)
+      throws IOException, MetricAgentException {
+    String line = reader.readLine();
     Matcher matcher = queryMetadata.matcher(line);
     if (matcher.find()) {
       userQuery.setQueryTime(matcher.group(1));
       userQuery.setLockTime(matcher.group(2));
       userQuery.setRowsSent(matcher.group(3));
       userQuery.setRowsExamined(matcher.group(4));
-      return true;
+    } else {
+      throw new MetricAgentException("Line (" + reader.getLineNumber() + ") "
+          + "did not match Query Metadata pattern: '" + line
+          + "' at " + matcher.toString());
     }
-    return false;
   }
 
-  static boolean parseSetUseStatment(String line) {
-    return setStatement.matcher(line).matches() || useStatement.matcher(line).matches();
+  static void parseSetStatement(RewindBufferedReader reader, UserQuery userQuery)
+      throws IOException, MetricAgentException {
+    String line = reader.readLine();
+    Matcher matcher = setStatement.matcher(line);
+    if (matcher.find()) {
+      userQuery.setTime(matcher.group(1));
+    } else {
+      throw new MetricAgentException("Line (" + reader.getLineNumber() + ") "
+          + "did not match Set Statement pattern: '" + line
+          + "' at " + matcher.toString());
+    }
   }
 
   static String replaceComments(String line) {
@@ -60,17 +71,59 @@ public class SlowQueryLogParser {
   }
 
   static boolean newSection(String line) {
-    return tsLine.matcher(line).matches() || uhLine.matcher(line).matches();
+    return tsLine.matcher(line).matches();
+  }
+
+  static boolean newQuerySection(String line) {
+    return uhLine.matcher(line).matches();
+  }
+
+  static UserQuery parseQuery(RewindBufferedReader bufferedReader)
+      throws IOException, MetricAgentException {
+
+    UserQuery query = new UserQuery();
+    parseUhLine(bufferedReader, query);
+    parseQueryMetadataLine(bufferedReader, query);
+    parseSetStatement(bufferedReader, query);
+    query.setQuery(replaceComments(bufferedReader.readLine()));
+    return query;
+  }
+
+  static List<UserQuery> parseTimeSection(RewindBufferedReader bufferedReader)
+      throws IOException, MetricAgentException {
+    //Skip first query section that run use statement
+    int headerLines = 5;
+    while (bufferedReader.ready() && headerLines > 0) {
+      bufferedReader.readLine();
+      headerLines--;
+    }
+
+    if (headerLines > 0) {
+      throw new MetricAgentException("Incomplete query section");
+    }
+
+    List<UserQuery> userQueries = new ArrayList<>();
+    String line = bufferedReader.readLine();
+    while (line != null && newQuerySection(line)) {
+      bufferedReader.rewind(line);
+      userQueries.add(parseQuery(bufferedReader));
+      line = bufferedReader.readLine();
+    }
+
+    if (line != null) {
+      bufferedReader.rewind(line);
+    }
+    return userQueries;
   }
 
   /**
    * Parses a MySql slow query log file and return a list of queries.
-   * @param is InputStream of the slow query log file
+   * @param br BufferReader of the slow query log file
    * @return A list of queries in the log file
    * @throws IOException An exception is thrown if the log file cannot be read successfully
    */
-  public static List<UserQuery> parseLog(InputStream is) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(is));
+  public static List<UserQuery> parseLog(RewindBufferedReader br)
+      throws IOException, MetricAgentException {
     List<UserQuery> userQueries = new ArrayList<>();
 
     //Read file header
@@ -81,43 +134,15 @@ public class SlowQueryLogParser {
     }
 
     if (headerLines > 0) {
-      throw new IOException("Incomplete slow query log");
+      throw new MetricAgentException("Incomplete slow query log");
     }
-    UserQuery userQuery = null;
 
     while (br.ready()) {
       String line = br.readLine();
       if (newSection(line)) {
-        if (userQuery != null) {
-          userQueries.add(userQuery);
-        }
-        userQuery = new UserQuery();
-        if (parseTsLine(line, userQuery)) {
-          if (!br.ready()) {
-            throw new IOException("Incomplete query log. Did not find CONNECTION LINE");
-          }
-          line = br.readLine();
-        }
-        if (parseUhLine(line, userQuery)) {
-          if (!br.ready()) {
-            throw new IOException("Incomplete query log. Did not find METADATA LINE");
-          }
-          line = br.readLine();
-        } else {
-          throw new IOException("Expected CONNECTION LINE but did not match");
-        }
-
-        if (!parseQueryMetadataLine(line, userQuery)) {
-          throw new IOException("Expected METADATA LINE but did not match");
-        }
-      } else if (userQuery == null) {
-        throw new IOException("Did not find a new query section");
-      } else if (!parseSetUseStatment(line)) {
-        userQuery.queries.add(replaceComments(line));
+        logger.debug("New Section at line: " + br.getLineNumber());
+        userQueries.addAll(parseTimeSection(br));
       }
-    }
-    if (userQuery != null) {
-      userQueries.add(userQuery);
     }
     return userQueries;
   }
