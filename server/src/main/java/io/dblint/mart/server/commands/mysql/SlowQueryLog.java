b6a811fca4d9300c98185fd73a1e6fdc3e21758b
Rajat Venkatesh <vrajat@dblint.io>
2019-03-13 12:27:30 +0530

    Organize command code

--- server/src/main/java/io/dblint/mart/server/commands/mysql/SlowQueryLog.java
+++ server/src/main/java/io/dblint/mart/server/commands/mysql/SlowQueryLog.java
@@ -1,6 +1,7 @@
 package io.dblint.mart.server.commands.mysql;
 
 import com.codahale.metrics.MetricRegistry;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.dblint.mart.analyses.mysql.QueryStats;
 import io.dblint.mart.analyses.mysql.SlowQuery;
 import io.dblint.mart.metricsink.mysql.RewindBufferedReader;
@@ -17,51 +18,50 @@ import net.sourceforge.argparse4j.inf.Subparser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.Reader;
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import javax.xml.stream.XMLStreamException;
 
-public class SlowQueryLog extends ConfiguredCommand<MartConfiguration> {
+public class SlowQueryLog extends LogParser {
   private static Logger logger = LoggerFactory.getLogger(SlowQueryLog.class);
+  private List<UserQuery> queries;
 
+  /**
+   * A command to parse slow query logs.
+   */
   public SlowQueryLog() {
     super("slowquerylog", "Analyze Queries in MySQL slow query log");
-  }
-
-
-  @Override
-  public void configure(Subparser subparser) {
+    this.queries = new ArrayList<>();
+    /*
     subparser.addArgument("-s", "--schemaFile")
         .metavar("schemaFile")
         .type(String.class)
         .help("Path to MySQL schema dump in XML");
+    */
+  }
 
-    subparser.addArgument("-l", "--slowQueryLog")
-        .metavar("slowQueryLog")
-        .type(String.class)
-        .help("Path to Slow Query Log");
+
+  @Override
+  protected void process(Reader reader)
+      throws IOException, MetricAgentException {
+
+    SlowQueryLogParser parser = new SlowQueryLogParser();
+    this.queries.addAll(parser.parseLog(
+        new RewindBufferedReader(reader)
+        )
+    );
   }
 
   @Override
-  protected void run(Bootstrap<MartConfiguration> bootstrap, Namespace namespace,
-                     MartConfiguration configuration)
-      throws XMLStreamException, IOException, QanException, MetricAgentException {
-    SchemaParser.Database database = SchemaParser.parseMySqlDump(
-        new FileInputStream(namespace.getString("schemaFile")));
-
-
-    logger.info(namespace.getString("slowQueryLog"));
-    SlowQueryLogParser parser = new SlowQueryLogParser();
-    List<UserQuery> queries = parser.parseLog(
-        new RewindBufferedReader(new InputStreamReader(
-            new FileInputStream(namespace.getString("slowQueryLog"))
-        ))
-    );
-
+  protected void output(OutputStream os) throws IOException {
+    ObjectMapper mapper = new ObjectMapper();
+    mapper.writeValue(os, queries);
+    /*
     MetricRegistry registry = new MetricRegistry();
     SlowQuery slowQuery = new SlowQuery(database, registry);
     slowQuery.analyze(queries);
@@ -76,5 +76,6 @@ public class SlowQueryLog extends ConfiguredCommand<MartConfiguration> {
 
     registry.getCounters()
       .forEach((name, counter) -> logger.info(name + ":" + counter.getCount()));
+    */
   }
 }
