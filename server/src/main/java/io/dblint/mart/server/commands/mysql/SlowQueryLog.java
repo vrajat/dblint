31788fab5d56e7a39e6b5e0fdfb98962b8d9bb2e
Rajat Venkatesh <vrajat@dblint.io>
2019-03-15 09:32:41 +0530

    add a slow query command test

--- server/src/main/java/io/dblint/mart/server/commands/mysql/SlowQueryLog.java
+++ server/src/main/java/io/dblint/mart/server/commands/mysql/SlowQueryLog.java
@@ -1,20 +1,10 @@
 package io.dblint.mart.server.commands.mysql;
 
-import com.codahale.metrics.MetricRegistry;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import io.dblint.mart.analyses.mysql.QueryStats;
-import io.dblint.mart.analyses.mysql.SlowQuery;
 import io.dblint.mart.metricsink.mysql.RewindBufferedReader;
-import io.dblint.mart.metricsink.mysql.SchemaParser;
 import io.dblint.mart.metricsink.mysql.SlowQueryLogParser;
 import io.dblint.mart.metricsink.mysql.UserQuery;
 import io.dblint.mart.metricsink.util.MetricAgentException;
-import io.dblint.mart.server.MartConfiguration;
-import io.dblint.mart.sqlplanner.QanException;
-import io.dropwizard.cli.ConfiguredCommand;
-import io.dropwizard.setup.Bootstrap;
-import net.sourceforge.argparse4j.inf.Namespace;
-import net.sourceforge.argparse4j.inf.Subparser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -22,10 +12,7 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.io.Reader;
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
-import java.util.Map;
-import javax.xml.stream.XMLStreamException;
 
 public class SlowQueryLog extends LogParser {
   private static Logger logger = LoggerFactory.getLogger(SlowQueryLog.class);
@@ -37,12 +24,6 @@ public class SlowQueryLog extends LogParser {
   public SlowQueryLog() {
     super("slowquerylog", "Analyze Queries in MySQL slow query log");
     this.queries = new ArrayList<>();
-    /*
-    subparser.addArgument("-s", "--schemaFile")
-        .metavar("schemaFile")
-        .type(String.class)
-        .help("Path to MySQL schema dump in XML");
-    */
   }
 
 
@@ -61,21 +42,5 @@ public class SlowQueryLog extends LogParser {
   protected void output(OutputStream os) throws IOException {
     ObjectMapper mapper = new ObjectMapper();
     mapper.writeValue(os, queries);
-    /*
-    MetricRegistry registry = new MetricRegistry();
-    SlowQuery slowQuery = new SlowQuery(database, registry);
-    slowQuery.analyze(queries);
-    Map<String, QueryStats> statsMap = slowQuery.getAggQueryStats();
-
-    logger.info("Sorted Queries");
-    statsMap.entrySet().stream()
-        .filter(entry -> entry.getValue().getNumQueries() > entry.getValue().getIndexUsed())
-        .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
-        .limit(50)
-        .forEach(entry -> logger.info(entry.getValue().toString()));
-
-    registry.getCounters()
-      .forEach((name, counter) -> logger.info(name + ":" + counter.getCount()));
-    */
   }
 }
